378076e09f64f3dfe5dea1b597c876c1edc32136
Drew <drewbdoo@gmail.com>
2022-06-13 12:14:48 -0400

    adding registerForm, fixing signupcontroller

--- src/main/java/Blog/controller/SignupController.java
+++ src/main/java/Blog/controller/SignupController.java
@@ -1,4 +1,30 @@
-package Blog.controller;//package com.springBootCRUD.Blog.controller;
-//
-//public class SignupController {
-//}
+package Blog.controller;
+
+import Blog.model.User;
+import Blog.service.UserService;
+import Blog.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SessionAttributes("user")
+public class SignupController {
+    private final UserService userService;
+    @Autowired
+            public SignupController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/signup")
+    public String getRegisterForm(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "registerForm";
+    }
+
+    @PostMapping("/register")
+    public User signup(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+}
